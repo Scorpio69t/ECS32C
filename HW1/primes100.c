2505753bb85f1f2ccac3a658e0be74eee93be8e7
Corey <coreychen2004@gmail.com>
2023-10-10 23:23:29 -0700

    LETS GOOO I FINALLY GOT THE PRIMES TO PRINT IDEK HOW

--- HW1/primes100.c
+++ HW1/primes100.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 int num_prime = 0; //The number of prime numbers so far
 int counter = 2; //The counter that increments
+int half_num; //The divisor that will be used to check for prime, won't go over half of counter
 
 int main() {
-    while (num_prime < 100) {
-        
-        for (int half_num = 2; half_num < counter/2; half_num++ ) {
+    for (counter = 2; num_prime < 100; counter++) {
+        int temp = 0;
+        for (half_num = 2; half_num <= counter/2; half_num++ ) {
             if (counter % half_num == 0) {
-                counter++;
+                temp++;
             }
         }
-        num_prime++;
-        if (num_prime % 10 == 0) {
-            printf("%d  \n", counter);
-        } 
-        else if(counter < 10){
-            printf("%d    ", counter);
+        if (temp == 0) {
+            num_prime++;
+            if (num_prime % 10 == 0) {
+                printf("%d  \n", counter);
+            } 
+            else if(counter < 10){
+                printf("%d    ", counter);
+            }
+            else if(counter < 100) {
+                printf("%d   ", counter);
+            }
+            else {
+                printf("%d  ", counter);
+            }
         }
-        else if(counter < 100) {
-            printf("%d   ", counter);
-        }
-        else {
-            printf("%d  ", counter);
-        }
-        counter++;
     }
     return 0;
 }
