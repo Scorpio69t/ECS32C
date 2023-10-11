702796b27b3dd604d98395e721cce8645eb1f0f0
Corey <coreychen2004@gmail.com>
2023-10-10 22:32:03 -0700

    turns out primes.c was not working, fixed the formatting, still have to fix primes, also added new hw files

--- HW1/primes100.c
+++ HW1/primes100.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int num_prime = 0; //The number of prime numbers so far
-int counter = 0; //The counter that increments
+int counter = 2; //The counter that increments
 
 int main() {
     while (num_prime < 100) {
@@ -8,15 +8,21 @@ int main() {
         for (int half_num = 2; half_num < counter/2; half_num++ ) {
             if (counter % half_num == 0) {
                 counter++;
-                break;
             }
         }
+        num_prime++;
         if (num_prime % 10 == 0) {
             printf("%d  \n", counter);
-        } else {
-            printf("%d", counter);
+        } 
+        else if(counter < 10){
+            printf("%d    ", counter);
         }
-        num_prime++;
+        else if(counter < 100) {
+            printf("%d   ", counter);
+        }
+        else {
+            printf("%d  ", counter);
+        }
         counter++;
     }
     return 0;
