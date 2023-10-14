563e18eeb0441a92ef9fbb7d64b8cc1949c228cd
cc81972 <coreychen.mp2@gmail.com>
2023-10-14 16:06:55 -0700

    oops forgot to save changes

--- HW1/most_matches.c
+++ HW1/most_matches.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) {
+    char mostOften;
+    int numTimes = 0;
+    for (int i = 0; i < strlength(argv[1]); i++) {
+        char tempChar = argv[1][i];
+        char tempNumTimes = 0;
+        for (int j = 0; j < strlength(argv[2]); i++) {
+            if (argv[2][j] == tempChar) {
+                tempNumTimes++;
+            }
+        }
+        if (tempNumTimes > numTimes) {
+            mostOften = tempChar;
+        }
+    }
+    printf("Most often : %c No. times: %d");
+    return 0;
+}
