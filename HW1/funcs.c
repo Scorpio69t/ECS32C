b5814bdaca3f21b17d400e0829e9cc72ababbab4
Corey <coreychen2004@gmail.com>
2023-10-14 17:37:09 -0700

    still need to work on returning 0 for funcs.c

--- HW1/funcs.c
+++ HW1/funcs.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 
-
-int between(char lowerBound, char upperBound, char target){
-// TODO: Implement.
-// The below return statement is here in order to prevent
-// a compiler error. You probably won't need it once you
-// implement this function.
+int between(char lowerBound, char upperBound, char target)
+{
+    // TODO: Implement.
+    // The below return statement is here in order to prevent
+    // a compiler error. You probably won't need it once you
+    // implement this function.
     int lowerBoundAscii = (int)lowerBound;
     int upperBoundAscii = (int)upperBound;
     int targetAscii = (int)target;
-    if (65 <= lowerBoundAscii <= 90 &&
-        65 <= upperBoundAscii <= 90 &&
-        65 <= targetAscii <= 90) {
-        if (lowerBoundAscii <= upperBoundAscii) {
-            if (lowerBoundAscii < targetAscii < upperBoundAscii) {
+    if (65 <= lowerBoundAscii && lowerBoundAscii <= 90 &&
+        65 <= upperBoundAscii && upperBoundAscii <= 90 &&
+        65 <= targetAscii && targetAscii <= 90)
+    {
+        if (lowerBoundAscii <= upperBoundAscii)
+        {
+            if (lowerBoundAscii <= targetAscii <= upperBoundAscii)
+            {
                 return 1;
-            } else {
+            }
+            else
+            {
                 return 0;
             }
-        } else {
+        }
+        else
+        {
             return -2;
         }
-    } else {
+    }
+    else
+    {
         return -1;
     }
 }
-
