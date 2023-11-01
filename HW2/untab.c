1cf9f9a30d77e273d0fc86decd49f1e18e2aaa6a
Corey <coreychen2004@gmail.com>
2023-11-01 11:40:11 -0700

    finished part 3, should be complete w homework

--- HW2/untab.c
+++ HW2/untab.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    
+    int tabSpace = 0;
+    int i = 0;
+    while(argv[1][i] != '\0') { //Convert to integer
+        tabSpace = (tabSpace * 10) + (argv[1][i] - '0');
+        i++;
+    }
+    char c;
+    int iter = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            int spacesTillNext;
+            spacesTillNext = tabSpace - iter;
+            for (int j = 0; j < spacesTillNext; j++) {
+                putchar(' ');
+                iter = 0;
+            }         
+        } else {
+            putchar(c);
+            if (c == '\n' || iter == tabSpace) {
+                iter = 0;
+            } else {
+                iter++;
+            }
+        }
+    }
+    return 0;
+
 }
